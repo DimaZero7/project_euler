82008453af34a02d77ae5a737f9e9f71b143c315
Dima Zero <dmitry.goptarev@gmail.com>
2023-01-02 11:08:47 +0300

    Added solution to problem 3

---
+++ id_3.py
@@ -0,0 +1,20 @@
+# https://projecteuler.net/problem=3
+
+
+def get_divisor(number: int):
+    for i in range(2, number + 1):
+        if number % i == 0:
+            return i
+
+
+number = 600851475143
+max_divisor = 0
+
+while number != 1:
+    divisor = get_divisor(number=number)
+    if divisor > max_divisor:
+        max_divisor = divisor
+
+    number = int(number / divisor)
+
+print(max_divisor)
