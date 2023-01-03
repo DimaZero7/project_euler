70c04515cf09191a7525523517bb532c0aa43b7b
Dima Zero <dmitry.goptarev@gmail.com>
2023-01-03 12:37:34 +0300

    Update solution to problem 4

--- id_4.py
+++ id_4.py
@@ -4,8 +4,8 @@
 largest_three_digit_number = 999
 max_product = 0
 
-for first_multiplier in range(1, largest_three_digit_number + 1):
-    for last_multiplier in range(1, largest_three_digit_number + 1):
+for first_multiplier in range(100, largest_three_digit_number + 1):
+    for last_multiplier in range(100, largest_three_digit_number + 1):
         product = first_multiplier * last_multiplier
         if str(product) == str(product)[::-1]:
             if max_product < product:
