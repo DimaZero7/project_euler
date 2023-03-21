79b953868df6723c115505a0c1d1d23c738fc26c
Dima Zero <dmitry.goptarev@gmail.com>
2023-03-21 10:40:33 +0300

    Solution to Problem 9 added

--- id_9.py
+++ id_9.py
@@ -1,10 +1,8 @@
 # https://projecteuler.net/problem=9
 # python 3.10
 
-
-for a in range(1, 1000):
-    for b in range(a + 1, 1000):
+for a in range(1, 1000 + 1):
+    for b in range(a + 1, 1000 + 1):
         c = 1000 - a - b
-        if c > 0 and a**2 + b**2 == c**2:
+        if a**2 + b**2 == c**2 and a + b + c == 1000:
             print(a * b * c)
-            break
