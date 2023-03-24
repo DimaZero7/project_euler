df51cc98004b5764796558ce9f99cd13a34ec7da
Dima Zero <dmitry.goptarev@gmail.com>
2023-03-24 18:32:41 +0300

    Solution to Problem 10 added

---
+++ id_10.py
@@ -0,0 +1,28 @@
+# https://projecteuler.net/problem=10
+# python 3.10
+
+limit = 2_000_000
+last_prime_number = 7
+prime_numbers = [2, 3, 5, last_prime_number]
+# maximum divisor of prime numbers at the moment
+maximum_divisor = 0
+
+while limit > last_prime_number:
+    # Step +2 to skip all multiples of 2
+    last_prime_number += 2
+
+    # We assume that the divisor of the next semisimple number will be in
+    #   the range from maximum_divisor to maximum_divisor + N
+    approximate_limit_maximum_divisor = maximum_divisor + 200
+
+    for prime_number in prime_numbers:
+        if prime_number >= approximate_limit_maximum_divisor:
+            prime_numbers.append(last_prime_number)
+            break
+        if last_prime_number % prime_number == 0:
+            maximum_divisor = max(maximum_divisor, prime_number)
+            break
+    else:
+        prime_numbers.append(last_prime_number)
+
+print(sum(prime_numbers))
