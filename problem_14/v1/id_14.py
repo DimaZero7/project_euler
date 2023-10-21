d32d22bf9dcff395d44d4d101b53afc8f72423cb
dmitry.goptarev <z.dima.zero@gmail.com>
2023-10-21 18:40:52 +0300

    added solution to problem 14

---
+++ problem_14/v1/id_14.py
@@ -0,0 +1,37 @@
+# https://projecteuler.net/problem=13
+# python 3.11
+
+import time
+
+
+start_time = time.time()
+
+end_number = 1_000_000
+max_count = 0
+max_number = 0
+number = 14
+
+for number in range(1, end_number + 1):
+    stop = False
+    number_count = 1
+    start_number = number
+
+    while not stop:
+        if number % 2 == 0:
+            number /= 2
+            number_count += 1
+        else:
+            number = number * 3 + 1
+            number_count += 1
+
+        if number == 1:
+            stop = True
+            if number_count > max_count:
+                max_count = number_count
+                max_number = start_number
+
+print(max_count, max_number)
+
+end_time = time.time()
+execution_time = end_time - start_time
+print("Execution time: {:.6f} seconds".format(execution_time))
